e03a864172bd3f8d268d8cd54b5b4ded6273ff41
ansuman <ansumanswain29@gmail.com>
2024-06-15 18:28:13 +0530

    add files

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import Bookmarks from './Components/Bookmarks/Bookmarks';
+import Todo from './Components/Todo/Todo';
+import Weather from './Components/Weather/Weather';
+import Greeting from './Components/Greeting/Greeting';
+import Time from './Components/Time/Time';
+
+function App() {
+  return (
+    <div className="app-wrapper">
+      <div className="App">
+        <div className="item1">
+          <Bookmarks />
+        </div>
+        <div className="item2">
+          <Time />
+        </div>
+        <div className="item3">
+          <Greeting />
+        </div>
+        <div className="item4">
+          <Weather />
+        </div>
+        <div className="item5">
+          <Todo />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
